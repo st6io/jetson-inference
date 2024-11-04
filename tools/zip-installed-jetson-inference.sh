59f25e2d849452acdbd0f8370223963ba03da0b9
Nikolay Valchev <niki@st6.io>
2024-11-04 17:45:02 +0200

    Fix zip-installed-jetson-inference script
    It seems that exclude artifacts contain some important header files.

--- tools/zip-installed-jetson-inference.sh
+++ tools/zip-installed-jetson-inference.sh
@@ -2,14 +2,6 @@
 
 build_dir="$(dirname "$0")"/../build
 
-exclude_artifacts=('/usr/local/share' '/usr/local/include')
-
-exclude_options=()
-for ext in "${exclude_artifacts[@]}"; do
-    exclude_options+=("-e "$ext"")
-done
-
-include_files="$(cat $build_dir/install_manifest.txt | grep -v ${exclude_options[@]})"
+include_files="$(cat $build_dir/install_manifest.txt)"
 
 zip "$build_dir/jetson-inference.zip" $include_files
-
